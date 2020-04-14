b634f3f991249a05d5c899cc51279175f391fd97
Agustin <itisanible@gmail.com>
2020-04-14 16:29:51 -0300

    Modificacion del menu.py, opcion de elegir idioma

--- menu.py
+++ menu.py
@@ -1,9 +1,23 @@
 def mainMenu():
 	import time
 	print('Date: '+time.strftime("%m-%d-%Y"))
-	print("What do you want to do? / ¿Qué quieres hacer?")
-	print("1- List of irregular nouns / Lista de sustantivos irregulares")
-	print("2- Know the plural of a noun / Saber el plural de un sustantivo")
-	print("3- Exit / salir")
 	del time
-	return input("Option? / ¿Opción?: ")
+	language=input('English or Spanish (enter E or S):').lower()
+	print('')
+	option=3
+	if language=='e':
+		print("What do you want to do? ")
+		print("1-List of irregular nouns")
+		print("2-Know the plural of a noun")
+		print("3-Exit")
+		option=input("Option?: ")
+	elif language=='s':
+		print("¿Qué quieres hacer?")
+		print("1-Lista de sustantivos irregulares")
+		print("2-Saber el plural de un sustantivo")
+		print("3-Salir")
+		option=input("Opcion?: ")
+	else:
+		print('Error!')
+
+	return option
